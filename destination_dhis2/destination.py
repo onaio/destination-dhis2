54bd059c99f7b64675f76e4c7a3f80d70e9c2d0e
Macharia Muguku <machariamuguku@gmail.com>
2023-02-24 11:09:03 +0300

    implement check - try requesting api for a small subset of data with provided configs

--- destination_dhis2/destination.py
+++ destination_dhis2/destination.py
@@ -5,16 +5,26 @@
 
 from typing import Any, Iterable, Mapping
 
+import requests
 from airbyte_cdk import AirbyteLogger
 from airbyte_cdk.destinations import Destination
-from airbyte_cdk.models import AirbyteConnectionStatus, AirbyteMessage, ConfiguredAirbyteCatalog, Status
+from airbyte_cdk.models import (
+    AirbyteConnectionStatus,
+    AirbyteMessage,
+    ConfiguredAirbyteCatalog,
+    Status,
+)
+
+from .dhis2_auth import Dhis2Authenticator
 
 
 class DestinationDhis2(Destination):
     def write(
-        self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
+        self,
+        config: Mapping[str, Any],
+        configured_catalog: ConfiguredAirbyteCatalog,
+        input_messages: Iterable[AirbyteMessage],
     ) -> Iterable[AirbyteMessage]:
-
         """
         TODO
         Reads the input stream of messages, config, and catalog to write data to the destination.
@@ -33,21 +43,46 @@ class DestinationDhis2(Destination):
 
         pass
 
-    def check(self, logger: AirbyteLogger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
-        """
-        Tests if the input configuration can be used to successfully connect to the destination with the needed permissions
-            e.g: if a provided API token or password can be used to connect and write to the destination.
+    def check(
+        self, logger: AirbyteLogger, config: Mapping[str, Any]
+    ) -> AirbyteConnectionStatus:
+        base_url = config["base_url"]
+        client_id = config["client_id"]
+        client_secret = config["client_secret"]
+        refresh_token = config["refresh_token"]
 
-        :param logger: Logging object to display debug/info/error to the logs
-            (logs will not be accessible via airbyte UI if they are not passed to this logger)
-        :param config: Json object containing the configuration of this destination, content of this json is as specified in
-        the properties of the spec.json file
-
-        :return: AirbyteConnectionStatus indicating a Success or Failure
-        """
         try:
-            # TODO
+            authenticator = Dhis2Authenticator(
+                token_refresh_endpoint=f"{base_url}/uaa/oauth/token",
+                client_id=client_id,
+                client_secret=client_secret,
+                refresh_token=refresh_token,
+            )
 
+            response = requests.get(
+                url=f"{base_url}/api/dataElements.json",
+                headers=authenticator.get_auth_header(),
+                # return smallest possible subset
+                params={
+                    "paging": "true",
+                    "page": 1,
+                    "pageSize": 1,
+                },
+            )
+
+            try:
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                try:
+                    error = response.json()["message"]
+                    logger.error(f"{error}:{e}")
+                    raise Exception(f"{error}:{e}")
+                except Exception as e:
+                    raise e
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+
         except Exception as e:
-            return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
+            logger.error(f"Error in check command: {e}")
+            return AirbyteConnectionStatus(
+                status=Status.FAILED, message=f"An exception occurred: {repr(e)}"
+            )
