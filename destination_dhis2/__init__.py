76514c2c5455cd89b88ac01c0d979574fcd78c46
Macharia Muguku <machariamuguku@gmail.com>
2023-03-08 16:59:06 +0300

    export DataValue and DataValues types

--- destination_dhis2/__init__.py
+++ destination_dhis2/__init__.py
@@ -4,7 +4,13 @@
 
 
 from .authenticator import Dhis2Authenticator
-from .client import Dhis2Client
+from .client import DataValue, DataValues, Dhis2Client
 from .destination import DestinationDhis2
 
-__all__ = ["DestinationDhis2", "Dhis2Authenticator", "Dhis2Client"]
+__all__ = [
+    "DestinationDhis2",
+    "Dhis2Authenticator",
+    "Dhis2Client",
+    "DataValue",
+    "DataValues",
+]
